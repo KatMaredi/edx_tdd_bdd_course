76d4ed280d06677aa3d395e9a46aabdb54ef065f
KatMaredi <katlego.maredi@enviro365.co.za>
2025-06-23 14:31:44 +0200

    Fix spelling mistake

--- moodule_3/practicing_tdd/models/Counter.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace practicing_tdd.models;
-
-public class Counter
-{
-    public string Name { get; set; }
-    public int Value { get; set; } = 0;
-}
