dccf4cfb06b713aae190a0534764e014db2c9e44
Fezao187 <katlegomaredi9@gmail.com>
2025-06-27 14:16:31 +0200

    Testing creating a product

--- finalProject/tdd_productRestApi/obj/Debug/net9.0/tdd_productRestApi.AssemblyInfo.cs
+++ finalProject/tdd_productRestApi/obj/Debug/net9.0/tdd_productRestApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tdd_productRestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf0d8375c0e3c97b1ebf74754deec4c004cca2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95d68b98ad1935b177952649d73ff46a69128a25")]
 [assembly: System.Reflection.AssemblyProductAttribute("tdd_productRestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tdd_productRestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- finalProject/tdd_productRestApi/tests/ProductsTest.cs
+++ finalProject/tdd_productRestApi/tests/ProductsTest.cs
@@ -17,5 +17,52 @@ public class ProductsTest
         var options = new DbContextOptionsBuilder<AppDbContext>()
             .UseNpgsql("Host=localhost;Port=5432;Database=productDb;Username=postgres;Password=Kristine")
             .Options;
+
+        _context = new AppDbContext(options);
+
+        _context.Database.EnsureCreated();
+    }
+
+    [OneTimeTearDown]
+    public void TeardownOnce()
+    {
+        _context.Dispose();
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        _context.Products.RemoveRange(_context.Products);
+        _context.SaveChanges();
+        product = new Product();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        _context.ChangeTracker.Clear();
+    }
+
+    /*******************************************************************************************************************
+     * TEST CASES
+     ******************************************************************************************************************/
+
+    [Test]
+    public void ShouldCreateAProduct()
+    {
+        product.Name = "Fedora";
+        product.Description = "A red hat";
+        product.Price = 12.50;
+        product.Available = true;
+        product.Category = Category.CLOTHS;
+        
+        Assert.That(product.ToString(),Is.EqualTo($"<Product name: Fedora, id: 0"));
+        Assert.That(product,Is.Not.Null);
+        Assert.That(product.Id,Is.EqualTo(0));
+        Assert.That(product.Name,Is.EqualTo("Fedora"));
+        Assert.That(product.Description,Is.EqualTo("A red hat"));
+        Assert.That(product.Price,Is.EqualTo(12.50));
+        Assert.That(product.Available,Is.EqualTo(true));
+        Assert.That(product.Category,Is.EqualTo(Category.CLOTHS));
     }
 }
